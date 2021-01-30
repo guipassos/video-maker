ccd39aa775298bd3e78e6be0bb4fe986690d0889
Guilherme C. Passos <guissos@gmail.com>
2021-01-30 00:32:10 -0300

    feat(text robot): start

---
+++ credentials/README.md
@@ -0,0 +1,11 @@
+# Credentials format
+
+## Algorithmia
+
+File: `algorithimia.json`
+
+```
+{
+  "apiKey": ""
+}
+```
--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const readline = require('readline-sync')
+const robots = {
+	text: require('./robots/text.js')
+}
 
-function start() {
-   const content = {}
+async function start() {
+	const content = {}
+	
+	content.searchTerm = askAndReturnSearchTerm()
+	content.prefix = askAndReturnPrefix()
 
-   content.searchTerm = askAndReturnSearchTerm()
-   content.prefix = askAndReturnPrefix()
+	await robots.text(content);
 
-   function askAndReturnSearchTerm() {
-      return readline.question('Type a Wikipedia search term: ')
-   }
+	function askAndReturnSearchTerm() {
+		return readline.question('Type a Wikipedia search term: ')
+	}
 
-   function askAndReturnPrefix() {
-      const prefixes = ['Who is', 'What is', 'The history of']
-      const selectedPrefixIndex = readline.keyInSelect(prefixes)
-      const selectedPrefixText = prefixes[selectedPrefixIndex]
-      
-      return selectedPrefixText
-   }
+	function askAndReturnPrefix() {
+		const prefixes = ['Who is', 'What is', 'The history of']
+		const selectedPrefixIndex = readline.keyInSelect(prefixes)
+		const selectedPrefixText = prefixes[selectedPrefixIndex]
+		
+		return selectedPrefixText
+	}
 
-   console.log(content)
+	console.log(content)
 }
 
 start()
